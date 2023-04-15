c6811f1fc171e75a50b7bd33932e64746c344963
SalterPendragon <121525881+SalterPendragon@users.noreply.github.com>
2023-04-15 19:44:11 -0300

    Update FartorialWHILE.py

--- FartorialWHILE.py
+++ FartorialWHILE.py
@@ -1,7 +1,11 @@
 numero = int(input("Digite o valor do fatorial: "))
 fat = 1
 aux = 1
-while aux <= numero:
-    fat *= aux
-    aux += 1
-print(fat)
+while numero <= 0:
+    print("Valor 0 ou negativos não são permitidos")
+    numero = int(input("Digite o valor do fatorial: "))
+if numero > 0:
+    while aux <= numero:
+        fat *= aux
+        aux += 1
+        print(fat)
